1a59a747e40f6390fece8b81ddc19133531d9902
Moon3301 <carlos_db_16@live.cl>
2025-01-16 12:10:37 -0300

    Agregar estructura inicial y componentes para la aplicación FormsApp

---
+++ Angular/formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  standalone: false,
+  
+  templateUrl: './dynamic-page.component.html',
+  styles: ``
+})
+export class DynamicPageComponent {
+
+}
---
+++ Angular/formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switches-page',
+  standalone: false,
+  
+  templateUrl: './switches-page.component.html',
+  styles: ``
+})
+export class SwitchesPageComponent {
+
+}
