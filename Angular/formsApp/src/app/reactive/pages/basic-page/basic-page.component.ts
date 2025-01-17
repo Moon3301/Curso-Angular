bb83a60dc4f063aedb3bd5528e248cf38a048e2d
Moon3301 <carlos_db_16@live.cl>
2025-01-17 11:05:04 -0300

    Mejorar la legibilidad del código en los componentes de la aplicación FormsApp y corregir la indentación en las clases de TypeScript

--- Angular/formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basic-page',
   standalone: false,
-  
+
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
+
 })
 export class BasicPageComponent {
 
--- Typescript/exercises-typescript/src/08/classes.ts
+++ Typescript/exercises-typescript/src/08/classes.ts
@@ -1,16 +1,15 @@
 export class Person {
 
-    constructor(public name: string, public address: string) {
-        this.name = name;
-        this.address = address; 
-    }
+  constructor(public name: string, public address: string) {
+    this.name = name;
+    this.address = address; 
+  }
 }
 
 export class Hero extends Person {
-
-    constructor(public alterEgo: string, public age: number, public realName:string){
-        super('tst','tessdd')
-    }
+  constructor(public alterEgo: string, public age: number, public realName:string){
+    super('tst','tessdd')
+  }
 }
 
 const ironman =  new Hero('Jhon', 45, 'Tony Stark');
