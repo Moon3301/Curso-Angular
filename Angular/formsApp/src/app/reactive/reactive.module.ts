8cfcf2188ace4f2dd58c4f586bb04701bf14c388
Moon3301 <carlos_db_16@live.cl>
2025-01-17 10:47:29 -0300

    Agregar menú lateral y mejorar la estructura de rutas en la aplicación FormsApp

--- Angular/formsApp/src/app/reactive/reactive.module.ts
+++ Angular/formsApp/src/app/reactive/reactive.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +15,9 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
+
   ]
 })
 export class ReactiveModule { }
